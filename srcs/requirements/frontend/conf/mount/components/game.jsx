30605eb79c48cf7d55dda5889e537fb8b5c7d35e
Mélanie Midon <melaniemidon02@gmail.com>
2024-05-15 14:56:51 +0200

    game is working AFTER reload

--- srcs/requirements/frontend/conf/mount/components/game.jsx
+++ srcs/requirements/frontend/conf/mount/components/game.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Head from 'next/head';
 
 function Pong({ initialWidth }) {
-  const [canvasWidth, setCanvasWidth] = useState(initialWidth);
+/*   const [canvasWidth, setCanvasWidth] = useState(initialWidth);
 
   const updateCanvasWidth = () => {
     const newWidth = window.innerWidth;
@@ -16,34 +16,21 @@ function Pong({ initialWidth }) {
     return () => {
       window.removeEventListener('resize', updateCanvasWidth);
     };
-  }, []);
+  }, []); */
 
   return (
     <div>
-      <Head>
-        <title>Pong</title>
-        <style jsx>{`
-          html, body {
-            background-color: black;
-            margin: 0;
-          }
-          #pong-canvas {
-            display: block;
-            margin: auto;
-          }
-        `}</style>
-      </Head>
-      <canvas id="pong-canvas" width={canvasWidth} height="500">
+      <canvas id="pong-canvas" width="1000" height="500">
         Votre navigateur ne supporte pas les canvas.
       </canvas>
       <script type="module" src="games/pong/pong.js"></script>
     </div>
   );
 }
-
+/*
 Pong.getInitialProps = async () => {
   // Récupérez la largeur de la fenêtre côté serveur
   return { initialWidth: 1000 }; // Définissez une valeur initiale pour la largeur
-};
+};*/
 
 export default Pong;
