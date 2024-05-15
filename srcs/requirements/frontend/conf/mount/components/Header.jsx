acd49e3c5e67f2a95b6679a16affd222a00b9c87
Mélanie Midon <melaniemidon02@gmail.com>
2024-05-15 13:24:24 +0200

    header stuff

--- srcs/requirements/frontend/conf/mount/components/Header.jsx
+++ srcs/requirements/frontend/conf/mount/components/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Link from 'next/link';
-import styles from '../styles/home.module.css';
+import styles from '../styles/header.module.css';
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Transcendence</Navbar.Brand>
+
+			<Navbar.Brand>
+			<Link href="/" passHref>
+                        <Nav.Link className={styles.logo}>Transcendence</Nav.Link>
+            </Link>
+			</Navbar.Brand>
+
                 <Nav className="mr-auto">
                     <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
                     <Nav.Link href="#how-to-play">How to play</Nav.Link>
                     <Nav.Link href="#credits">Credits</Nav.Link>
                 </Nav>
-                <Nav className="ml-auto"> {/* Updated to align items to the right */}
+                <Nav className="mr-auto">
                     <NavDropdown title={<img src="/images/setting.png" alt="User Menu" className={styles.settingIcon} />} id="basic-nav-dropdown">
                         <Link href="/profile" passHref>
                             <NavDropdown.Item as="a">My Profile</NavDropdown.Item>
