79cb8b15bfb691112be28661f82aaf864a6d5738
Mélanie Midon <melaniemidon02@gmail.com>
2024-05-17 15:55:53 +0200

    middleware finished

--- srcs/requirements/frontend/conf/mount/middleware.js
+++ srcs/requirements/frontend/conf/mount/middleware.js
@@ -2,8 +2,9 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req) {
     const isLoginPage = req.url.includes('/login') || req.url.includes('/register');
+    const isLoggedIn = req.cookies.get('refresh');
 
-    if (!isLoginPage) {
+    if (!isLoginPage && !isLoggedIn) {
         const loginUrl = new URL('/account/login', `http://localhost:50281`);
         return NextResponse.redirect(loginUrl.href);
     }
