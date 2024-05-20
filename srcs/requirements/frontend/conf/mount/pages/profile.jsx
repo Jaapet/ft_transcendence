bb3218580243ecc1d264c4961281a7b782a4cc4f
Your mmidon <you@example.com>
2024-05-20 15:44:19 +0200

    uglyprofile working

--- srcs/requirements/frontend/conf/mount/pages/profile.jsx
+++ srcs/requirements/frontend/conf/mount/pages/profile.jsx
@@ -2,51 +2,32 @@ import React, { useContext } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/profile.module.css';
-import {useRouter} from 'next/router'
 import AuthenticationContext from '../context/AuthenticationContext';
 
+const Profile = () => {
+    const { user } = useContext(AuthenticationContext);
 
-const Profile = ({ profile }) => {
-	const router = useRouter();
+    if (!user) {
+        return <p>Veuillez vous connecter pour accéder à votre profil.</p>;
+    }
 
-	if (!profile) {
-		router.push('/errorNotFound');
-		return null;
-	  }
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Profile - Transcendence</title>
-      </Head>
-      <h1>My Profile</h1>
-      <div className={styles.profileDetails}>
-        <Image src={profile.avatar} alt="Profile Picture" width={150} height={150} className={styles.profilePicture} />
-        <div className={styles.info}>
-          <p>Name: {profile.username}</p>
-          <p>Email: {profile.email}</p>
-          <p>Bio: {profile.bio}</p>
-          <p>Joined on: {profile.join_date}</p>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Profile - Transcendence</title>
+            </Head>
+            <h1>My Profile</h1>
+            <div className={styles.profileDetails}>
+                <Image src={user.avatar} alt="Profile Picture" width={150} height={150} className={styles.profilePicture} />
+                <div className={styles.info}>
+                    <p>Name: {user.username}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Bio: {user.bio}</p>
+                    <p>Joined on: {user.join_date}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
-
-export async function getServerSideProps() {
-	const user = AuthenticationContext.user;
-
-	console.log(user);
-  if (!user) {
-    return {
-      props: { profile: null },
-    };
-  }
-
-  const profile = await res.json();
-
-  return {
-    props: { profile },
-  };
-}
 
 export default Profile;
