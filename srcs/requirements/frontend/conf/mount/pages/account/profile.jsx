65e7978801db9163b09c248d0b01bfddc7005f5b
Your mmidon <you@example.com>
2024-05-21 18:59:35 +0200

    c'est fou a quel point ils sont beau mes boutons n'est ce pas ?

--- srcs/requirements/frontend/conf/mount/pages/account/profile.jsx
+++ srcs/requirements/frontend/conf/mount/pages/account/profile.jsx
@@ -36,25 +36,28 @@ const Profile = () => {
 		<div>
 			<Header />
 			<div className={styles.container}>
-				<Head>
-					<title>Profile Page</title>
-				</Head>
+				<Head><title>Profile Page</title></Head>
+				
 				<h1 className={`mt-3 ${styles.background_title}`}>{user.username}</h1>
+				
 				<div className="row">
 					<div className="col-md-4">
 						<div className={`card ${styles.customCard}`}>
 							<Image src={user.avatar} alt="Profile Picture" width={150} height={150} className="card-img-top" />
 							<div className="card-body">
-								<h5 className="card-title">{user.username}</h5>
+								<h3 className="card-title">{user.username}</h3>
 								{/* TODO: Add ELO here */}
 								<p className="card-text"><small className="text-muted">Joined on: {user.join_date}</small></p>
 							</div>
 						</div>
 					</div>
+
+
 					<div className="col-md-8">
 						<div className={`card ${styles.customCard}`}>
-				<div className={`card ${styles.customCard}`}>
-					<div className="card-body">
+							<div className={`card ${styles.customCard}`}>
+								<div className="card-body">
+						
 						<h5 className="card-title">Last Matches</h5>
 						<ul className="list-group list-group">
 							{last_matches.map(match => (
@@ -73,6 +76,7 @@ const Profile = () => {
 						<p><a href="/account/match-history" className="link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">See full match history</a></p>
 						</div>
 					</div>
+					
 							<div className="card-body">
 								<h5 className="card-title">Contact Information</h5>
 								<p className="card-text">Email: {user.email}</p>
