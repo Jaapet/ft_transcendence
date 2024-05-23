619d86c2a0f4a17941960994727012d92b0ee65c
Your mmidon <you@example.com>
2024-05-23 14:38:20 +0200

    default header for every pages

--- srcs/requirements/frontend/conf/mount/pages/_app.js
+++ srcs/requirements/frontend/conf/mount/pages/_app.js
@@ -1,13 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React from 'react'
 import { AuthenticationProvider } from '../context/AuthenticationContext'
+import  Header  from '../components/Header'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<AuthenticationProvider>
+			<Header/>
 			<Component {...pageProps} />
 		</AuthenticationProvider>
+
 	)
 }
 
 export default MyApp
+
