17c8a9fa1ca3db119052a64a63c51f3b8abc1002
Your mmidon <you@example.com>
2024-06-02 18:20:11 +0200

    fixed friend things

--- srcs/requirements/frontend/conf/mount/pages/users/[id]/friends.jsx
+++ srcs/requirements/frontend/conf/mount/pages/users/[id]/friends.jsx
@@ -96,7 +96,7 @@ Match objects contain:
 			return (
 				<div className={`card ${styles.customCard}`}>
 					<div className="card-body">
-						<h5 className="card-title mb-0">You have no friends 🤭🫵</h5>
+						<h2 className="card-title mb-0">You have no friends 🤭🫵</h2>
 						<img src="/images/sadboy.png" alt="Royal Icon" style={{ width: '50%', height: 'auto' }}></img>
 					</div>
 				</div>
