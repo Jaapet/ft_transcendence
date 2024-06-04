401070a83f31c0b84aaa1d6a98c0a7a0a64c1878
thepaqui <theopaquier2@gmail.com>
2024-06-04 22:50:35 +0200

    Can now edit profile!

---
+++ srcs/requirements/frontend/conf/mount/pages/api/current_user/edit.jsx
@@ -0,0 +1,79 @@
+import cookie from 'cookie';
+import { IncomingForm } from 'formidable';
+import fs from 'fs';
+
+export const config = {
+	api: {
+		bodyParser: false, // Disable Next.js built-in body parser
+	},
+};
+
+const parseForm = (req) =>
+	new Promise((resolve, reject) => {
+		const form = new IncomingForm();
+		form.parse(req, (err, fields, files) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve({ fields, files });
+			}
+		});
+	});
+
+export default async (req, res) => {
+	// Only PUT allowed
+	if (req.method !== 'PUT') {
+		res.setHeader('Allow', ['PUT']);
+		return res.status(405).json({ message: `Method ${req.method} is not allowed` });
+	}
+
+	try {
+		if (!req.headers.cookie) {
+			throw new Error('Unauthorized');
+		}
+		const { access } = cookie.parse(req.headers.cookie);
+		if (!access) {
+			// TODO: make a refresh function
+			throw new Error('Could not fetch access token');
+		}
+
+		let formData = null;
+		const { fields, files } = await parseForm(req);
+
+		formData = new FormData();
+		formData.append('username', fields.username);
+		formData.append('email', fields.email);
+		formData.append('password', fields.password);
+		if (files.avatar) {
+			const file = files.avatar[0];
+			const fileBuffer = fs.readFileSync(file.filepath);
+			const blob = new Blob([fileBuffer], { type: file.mimetype });
+			formData.append('avatar', blob, file.originalFilename);
+		}
+
+		// Edit user
+		const response = await fetch(`http://backend:8000/api/edit/`, {
+			method: 'PUT',
+			headers: {
+				'Authorization': `Bearer ${access}`
+			},
+			body: formData
+		});
+		if (!response) {
+			throw new Error('Edit failed');
+		}
+
+		const data = await response.json();
+		if (!data) {
+			throw new Error('Edit failed');
+		}
+		if (!response.ok) {
+			throw new Error(data.username || data.email || data.password || data.avatar || 'Edit failed');
+		}
+
+		return res.status(200).json({ message: 'User has been edited' });
+	} catch (error) {
+		console.error('API EDIT:', error);
+		return res.status(500).json({ message: error.message });
+	}
+}
