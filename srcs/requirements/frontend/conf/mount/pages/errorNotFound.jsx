b65a81ffa62aab0cf5c5a2940ec82295ed6abb7f
Your mmidon <you@example.com>
2024-06-18 19:59:53 +0200

    error page done

--- srcs/requirements/frontend/conf/mount/pages/errorNotFound.jsx
+++ srcs/requirements/frontend/conf/mount/pages/errorNotFound.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import styles from '../styles/error.module.css';
+import styles from '../styles/base.module.css';
 
 const ErrorPage = () => {
   return (
     <div className={styles.container}>
-      <p>Oops! Something went wrong.</p>
-      <p>We couldn't retrieve the information at the moment. Please try again later.</p>
-    </div>
+		
+		<h5 className={styles.background_title}> Oops </h5>
+
+		<div className={styles.cardInfo}>
+			<p>Something went wrong.</p>
+			<p>We couldn't retrieve requested informations at the moment.</p>
+			<p>Here's some pretty stuff to make you wait : </p>
+			
+			<div className={styles.buttonContainer} >
+
+			<img src="images/uranus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+			<img src="images/saturnus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+			<img src="images/flowers.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+
+			</div>
+
+			<p style={{position: 'absolute', bottom: '1.5cm'}}>Reload the page might resolve your problem but I can't promise.</p>
+    	</div>
+    
+	</div>
   );
 };
 
