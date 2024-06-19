6a22675c7cbf6a217d51839789ad36232863a885
thepaqui <theopaquier2@gmail.com>
2024-06-19 14:34:23 +0200

    Added lower and upper bounds for Pong ELO

--- srcs/requirements/backend/conf/mount/game/signals.py
+++ srcs/requirements/backend/conf/mount/game/signals.py
@@ -17,7 +17,7 @@ def delete_match_if_both_players_deleted(sender, instance, **kwargs):
 
 # ELO
 # TODO: Make ELO for Match3
-def calculate_pong_elo(winner_elo, loser_elo, k=40):
+def calculate_pong_elo(winner_elo, loser_elo, k=40, min_elo=0, max_elo=5000):
 	# expected_win is the expected probability that the winner won
 	expected_win = 1 / (1 + 10 ** ((loser_elo - winner_elo) / 400))
 	# expected_lose is the expected probability that the winner lost
@@ -26,6 +26,9 @@ def calculate_pong_elo(winner_elo, loser_elo, k=40):
 	# 1 means winning, 0 means losing
 	new_winner_elo = winner_elo + k * (1 - expected_win)
 	new_loser_elo = loser_elo + k * (0 - expected_lose)
+
+	new_winner_elo = min(max(new_winner_elo, min_elo), max_elo)
+	new_loser_elo = min(max(new_loser_elo, min_elo), max_elo)
 
 	return new_winner_elo, new_loser_elo
 
