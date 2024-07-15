65d38d70651d3623f87de1773ca2c3d05e7200c7
thepaqui <theopaquier2@gmail.com>
2024-07-15 15:34:52 +0200

    incorporated drawing canvas with game UI

--- srcs/requirements/frontend/conf/mount/components/pong.jsx
+++ srcs/requirements/frontend/conf/mount/components/pong.jsx
@@ -916,7 +916,7 @@ const Pong = ({ scoreL, setScoreL, scoreR, setScoreR, gameEnd, setGameEnd, setWi
 		<div>
 			{testmessage}
 			<div className={styles.canvasWrapper}>
-				<canvas hidden={hidden} ref={canvasRef} id="pong-canvas" className={styles.canvas} width="1080" height="720"></canvas>
+				<canvas hidden={hidden} ref={canvasRef} id="pong-canvas" className={styles.canvas} width="800" height="600"></canvas>
 			</div>
 		</div>
 	);
