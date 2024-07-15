9153e2bdc5dfaead061f0fa6863f102183437236
thepaqui <theopaquier2@gmail.com>
2024-07-15 16:00:40 +0200

    Reworded 404 page

--- srcs/requirements/frontend/conf/mount/pages/404.jsx
+++ srcs/requirements/frontend/conf/mount/pages/404.jsx
@@ -2,29 +2,22 @@ import React from 'react';
 import styles from '../styles/base.module.css';
 
 const ErrorPage = () => {
-  return (
-    <div className={styles.container}>
-		
-		<h5 className={styles.background_title}> Oops </h5>
-
-		<div className={styles.cardInfo}>
-			<p>Something went wrong.</p>
-			<p>We couldn't retrieve requested informations at the moment.</p>
-			<p>Here's some pretty stuff to make you wait : </p>
-			
-			<div className={styles.buttonContainer} >
-
-			<img src="images/uranus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
-			<img src="images/saturnus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
-			<img src="images/flowers.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
-
+	return (
+		<div className={styles.container}>
+			<h5 className={styles.background_title}>Oops!</h5>
+			<div className={styles.cardInfo}>
+				<p>Something went wrong.</p>
+				<p>We couldn't retrieve the requested resource.</p>
+				<p>Here's some pretty stuff to apologize:</p>
+				<div className={styles.buttonContainer} >
+					<img src="images/uranus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+					<img src="images/saturnus.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+					<img src="images/flowers.webp" alt="ArrowsGif" style={{height: '5cm'}}/>
+				</div>
+				<p style={{position: 'absolute', bottom: '1.5cm'}}>Reloading the page might resolve the problem.</p>
 			</div>
-
-			<p style={{position: 'absolute', bottom: '1.5cm'}}>Reload the page might resolve your problem but I can't promise.</p>
-    	</div>
-    
-	</div>
-  );
+		</div>
+	);
 };
 
 export default ErrorPage;
