7c72f8a1d17ca8f1259ad7f38ef8ab5165b34bc4
thepaqui <theopaquier2@gmail.com>
2024-08-20 16:25:33 +0200

    Deployment fix

--- srcs/requirements/backend/conf/mount/game/apps.py
+++ srcs/requirements/backend/conf/mount/game/apps.py
@@ -1,10 +1,15 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
+from django.core.exceptions import ImproperlyConfigured
 
 class UsersConfig(AppConfig):
 	default_auto_field = 'django.db.models.BigAutoField'
 	name = 'game'
 
 	def ready(self):
-		import game.signals
-		from .models import Member
-		Member.objects.all().update(is_ingame=False)
+		try:
+			Member = apps.get_model('game', 'Member')
+			Member.objects.all().update(is_ingame=False)
+		except LookupError:
+			print("Member model does not yet exist")
+		except ImproperlyConfigured as e:
+			print(f"ImproperlyConfigured: {e}")
