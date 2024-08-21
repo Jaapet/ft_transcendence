4d61a03e7b7d33d29a664e7cb12d548e5fae4a82
thepaqui <theopaquier2@gmail.com>
2024-08-21 16:56:13 +0200

    Tooltips for status circle

--- srcs/requirements/frontend/conf/mount/components/StatusCircle.jsx
+++ srcs/requirements/frontend/conf/mount/components/StatusCircle.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 const StatusCircle = ({ status }) => {
+
+	let hoverTip = '';
+
+	switch (status) {
+		case 'online':
+			hoverTip = 'Online';
+			break ;
+		case 'ingame':
+			hoverTip = 'In game';
+			break ;
+		case 'offline':
+		default:
+			hoverTip = 'Offline';
+			break ;
+	}
+
+	const tooltip = (
+		<Tooltip id='tooltip'>
+			{hoverTip}
+		</Tooltip>
+	);
+
 	return (
-		<span
-			style={{
-				display: 'inline-block',
-				width: '15px',
-				height: '15px',
-				borderRadius: '50%',
-				backgroundColor: status === "offline" ? 'red' : status === "ingame" ? 'blueviolet' : 'green' ,
-				marginRight: '5px',
-				verticalAlign: 'middle'
-			}}
-		>
-		</span>
+		<OverlayTrigger placement="top" overlay={tooltip}>
+			<span
+				style={{
+					display: 'inline-block',
+					width: '15px',
+					height: '15px',
+					borderRadius: '50%',
+					backgroundColor: status === "offline" ? 'red' : status === "ingame" ? 'blueviolet' : 'green' ,
+					marginRight: '5px',
+					verticalAlign: 'middle'
+				}}
+			>
+			</span>
+		</OverlayTrigger>
 	);
 };
 
