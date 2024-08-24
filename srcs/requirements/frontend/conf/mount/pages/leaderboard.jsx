579cfc8188ddbae43f3de1a4559bc45f5c5bf357
thepaqui <theopaquier2@gmail.com>
2024-08-24 21:11:16 +0200

    Better leaderboard page

--- srcs/requirements/frontend/conf/mount/pages/leaderboard.jsx
+++ srcs/requirements/frontend/conf/mount/pages/leaderboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Card, Col, Row, Table } from 'react-bootstrap';
-import Link from 'next/link';
 import styles from '../styles/base.module.css';
 import { useAuth } from '../context/AuthenticationContext';
-
-const LeaderboardTableRow = ({ player, index, type }) => {
-	//console.log(player); // debug
+import LeaderboardTable from '../components/LeaderboardTable';
+/*
+import { Card, Col, Row, Table } from 'react-bootstrap';
+import Link from 'next/link';
+const LeaderboardTableRow = ({ player, index }) => {
 	return (
 		<tr key={index} className={`list-group-item ${styles.customList} d-flex justify-content-between`}>
 			<th>{index}</th>
@@ -13,17 +13,17 @@ const LeaderboardTableRow = ({ player, index, type }) => {
 				<Link
 					href={`/users/${player.id}`}
 					passHref
-					style={{color: '#38255faa', texthecoration: 'none'}}
+					style={{color: '#38255faa', textDecoration: 'none'}}
 				>
 					{player.username}
 				</Link>
 			</th>
-			<th>{type === 'pong' ? player.elo_pong : player.elo_royal}</th>
+			<th>{player.elo_pong}</th>
 		</tr>
 	);
 }
 
-const LeaderboardTable = ({ title, leaders, type }) => {
+const LeaderboardTable = ({ title, leaders }) => {
 	return (
 		<Card className={styles.customCard}>
 			<Card.Body>
@@ -39,7 +39,7 @@ const LeaderboardTable = ({ title, leaders, type }) => {
 					</thead>
 					<tbody>
 						{leaders.map((player, index) => (
-							<LeaderboardTableRow player={player} index={index + 1} type={type} />
+							<LeaderboardTableRow player={player} index={index + 1} />
 						))}
 					</tbody>
 				</table>
@@ -48,8 +48,8 @@ const LeaderboardTable = ({ title, leaders, type }) => {
 		</Card>
 	);
 }
-
-export default function Leaderboard({ status, detail, pong, royal }) {
+*/
+export default function Leaderboard({ status, detail, pong }) {
 	const { logout } = useAuth();
 
 	const handleLogout = async () => {
@@ -69,24 +69,26 @@ export default function Leaderboard({ status, detail, pong, royal }) {
 		);
 	}
 
-	//console.log('pong', pong); // debug
-	//console.log('royal', royal); // debug
-
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.background_title} style={{margin: '20px'}}>Leaderboard</h1>
+			<LeaderboardTable leaders={pong} />
+		</div>
+	);
+/*
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.background_title} style={{margin: '20px'}}>Leaderboards</h1>
 			<Card className={styles.backCard} style={{}}>
 				<Row>
 					<Col md={6}>
-						<LeaderboardTable title={'Classic Pong Players'} leaders={pong} type={'pong'} />
-					</Col>
-					<Col md={6}>
-						<LeaderboardTable title={'Royal Pong Players'} leaders={royal} type={'royal'} />
+						<LeaderboardTable title={'Pong Leaderboard'} leaders={pong} />
 					</Col>
 				</Row>
 			</Card>
 		</div>
 	);
+*/
 }
 
 export async function getServerSideProps(context) {
@@ -107,8 +109,7 @@ export async function getServerSideProps(context) {
 				props: {
 					status: 404,
 					detail: 'Resource not found',
-					pong: null,
-					royal: null
+					pong: null
 				}
 			}
 		}
@@ -125,18 +126,15 @@ export async function getServerSideProps(context) {
 			props: {
 				status: 200,
 				detail: 'Success',
-				pong: data.pong,
-				royal: data.royal
+				pong: data.pong
 			}
 		}
-	} catch (error) {
-		//console.error('USER LEADERBOARDS:', error);
+	} catch (error) { 
 		return {
 			props: {
 				status: 401,
 				detail: error.message,
-				pong: null,
-				royal: null
+				pong: null
 			}
 		}
 	}
